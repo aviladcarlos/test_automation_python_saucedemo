c48a8f425109b65dc5ac2bb4686168071f186379
aviladcarlos <aviladcarlos@gmail.com>
2024-12-13 16:50:23 -0600

    module to test if a standard user is able to log in

---
+++ tests/test_login_standard_user.py
@@ -0,0 +1,32 @@
+import pytest
+
+import time
+
+from pageobjects.login_page_objects import LoginPageObjects
+from testdata.login_page_data import LoginPageData
+from utilities.baseclass import BaseClass
+
+
+class TestLoginStandardUser(BaseClass):
+
+    def test_login_standard_user(self, login_page_data):
+
+        log = self.get_logger()
+        login_page = LoginPageObjects(self.driver)
+
+        log.info(f"Entering '{login_page_data["username"]}' for username")
+        login_page.username_field().send_keys(login_page_data["username"])
+
+        log.info(f"Entering password for '{login_page_data["username"]}'")
+        login_page.password_field().send_keys(login_page_data["password"])
+
+        log.info("Clicking 'Login' btn")
+        products_page = login_page.login_btn()
+
+        # Verifying user has successfully logged in by checking if they have landed on the Products page.
+        assert "Products" in products_page.page_title().text, log.critical("Test failed: user did not login successfully")
+        log.info("User logged in successfully")
+
+    @pytest.fixture(params=LoginPageData.standard_user)
+    def login_page_data(self, request):
+        return request.param
