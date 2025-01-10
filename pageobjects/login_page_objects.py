73479039648e17635375ce9d6e293a2585c51737
aviladcarlos <aviladcarlos@gmail.com>
2025-01-10 15:53:07 -0600

    Added additional elements to test a locked out user

--- pageobjects/login_page_objects.py
+++ pageobjects/login_page_objects.py
@@ -2,12 +2,15 @@ from selenium.webdriver.common.by import By
 
 from pageobjects.products_page_objects import ProductsPageObjects
 
+
 class LoginPageObjects:
 
     loginlogo = (By.CSS_SELECTOR, ".login_logo")
     usernamefield = (By.ID, "user-name")
     passwordfield = (By.ID, "password")
     loginbtn = (By.ID, "login-button")
+    errormsg = (By.CSS_SELECTOR, "[data-test='error']")
+    loginbox = (By.CLASS_NAME, "login-box")
 
     def __init__(self, driver):
         self.driver = driver
@@ -25,3 +28,13 @@ class LoginPageObjects:
         self.driver.find_element(*LoginPageObjects.loginbtn).click()
         return ProductsPageObjects(self.driver)
 
+    def error_msg(self):
+        return self.driver.find_element(*LoginPageObjects.errormsg)
+
+    # Using find_element would trigger a 'NoSuchElementException' error which can be handled with a try block. If I use
+    # find_elements instead, it returns an empty list and won't trigger an error when no element is found.
+    # The empty considered is false when checked, so I can compare against it to determine if the element exists.
+    # I decided to use find_elements as it would take fewer lines to check if the element exists compared to using a
+    # try block to handle the NoSuchElementException with find_element.
+    def login_box(self):
+        return self.driver.find_elements(*LoginPageObjects.loginbox)
