0db3bc3cdc2804ae3b36ec45c913679e8fae4c52
aviladcarlos <aviladcarlos@gmail.com>
2025-01-29 16:11:33 -0600

    Created expected products listed to test against.

---
+++ testdata/products_page_data.py
@@ -0,0 +1,14 @@
+class ProductsPageData:
+    expected_products = {"Sauce Labs Backpack": {"image": "https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg",
+                                                 "cost": "$29.99"},
+                         "Sauce Labs Bike Light": {"image": "https://www.saucedemo.com/static/media/bike-light-1200x1500.37c843b0.jpg",
+                                                   "cost": "$9.99"},
+                         "Sauce Labs Bolt T-Shirt": {"image": "https://www.saucedemo.com/static/media/bolt-shirt-1200x1500.c2599ac5.jpg",
+                                                     "cost": "$15.99"},
+                         "Sauce Labs Fleece Jacket": {"image": "https://www.saucedemo.com/static/media/sauce-pullover-1200x1500.51d7ffaf.jpg",
+                                                      "cost": "$49.99"},
+                         "Sauce Labs Onesie": {"image": "https://www.saucedemo.com/static/media/red-onesie-1200x1500.2ec615b2.jpg",
+                                               "cost": "$7.99"},
+                         "Test.allTheThings() T-Shirt (Red)": {"image": "https://www.saucedemo.com/static/media/red-tatt-1200x1500.30dadef4.jpg",
+                                                               "cost": "$15.99"},
+                         }
