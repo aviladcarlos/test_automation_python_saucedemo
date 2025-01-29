068e6dc9fcde79e94fd85e8e05a2a5bab866ea25
aviladcarlos <aviladcarlos@gmail.com>
2025-01-29 16:10:43 -0600

    Updated with locators for products and product details.

--- pageobjects/products_page_objects.py
+++ pageobjects/products_page_objects.py
@@ -4,9 +4,18 @@ from selenium.webdriver.common.by import By
 class ProductsPageObjects:
 
     pagetitle = (By.CSS_SELECTOR, ".title")
+    productlist = (By.CSS_SELECTOR, ".inventory_item")
+    # Chains with productlist:
+    productlist_name = (By.CLASS_NAME, "inventory_item_name")
+    productlist_image = (By.CSS_SELECTOR, "img")
+    productlist_price = (By.CLASS_NAME, "inventory_item_price")
+
 
     def __init__(self, driver):
         self.driver = driver
 
     def page_title(self):
         return self.driver.find_element(*ProductsPageObjects.pagetitle)
+
+    def product_list(self):
+        return self.driver.find_elements(*ProductsPageObjects.productlist)
